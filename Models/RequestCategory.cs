b80913ee21d5876680c5a816d4218b5a1ccfac52
Your Name <you@example.com>
2022-12-20 16:28:03 +0700

    Add migrations

---
+++ Models/RequestCategory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppDev.Models
+{
+    public class RequestCategory
+    {
+        public int Id { get; set; }
+
+        public string StoreOwnerId { get; set; } = null!;
+        
+        [ValidateNever]
+        [Display(Name = "Store Owner")]
+        public ApplicationUser StoreOwner { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Is Appproved")]    
+        public bool? IsAppproved { get; set; } = null;
+
+        [StringLength(255)]
+        public string Message { get; set; } = "";
+
+        public bool IsEditable(string storeOwnerId)
+        {
+            if (storeOwnerId != this.StoreOwnerId)
+                return false;
+            if (IsAppproved != null)
+                return false;
+            return true;
+        }
+
+        public bool IsApprovable()
+        {
+            if (IsAppproved == true)
+                return false;
+            return true;
+        }
+    }
+}
